a7d005b18d1d95013dc8a7e1f2f299f5dcd2f4e6
Mirko Mariotti <mirko@mirkomariotti.it>
2024-06-19 09:32:32 +0200

    config files

---
+++ bellstate/bmqsim_hw/config.py
@@ -0,0 +1,4 @@
+ssh_key="/home/mirko/.ssh/structural"
+ssh_user="root"
+ssh_host="zedbm"
+work_dir="/root/quantum/proj_zedboard_quantum_bellstate"
---
+++ xgate/bmqsim_alveo/config.py
@@ -0,0 +1,4 @@
+ssh_key="/home/mirko/.ssh/structural"
+ssh_user="mirko"
+ssh_host="vitis2"
+work_dir="/home/mirko/alveoruns/proj_alveou55c_xgate/"
